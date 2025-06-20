3e0a94313cb4bce9599ef59e7c6779714005bc69
serweryn617 <seweryn.r.1999@gmail.com>
2025-06-20 15:47:38 +0200

    Add I2C driver

---
+++ firmware/lib/defs/inc/defs/defs.hpp
@@ -0,0 +1,16 @@
+#ifndef LIB_DEFS_DEFS_HPP
+#define LIB_DEFS_DEFS_HPP
+
+#include <cstdint>
+
+namespace defs::i2c {
+
+constexpr uint8_t inst = 1;
+constexpr uint8_t scl_pin = 3;
+constexpr uint8_t sda_pin = 2;
+constexpr uint32_t baudrate = 400'000;
+constexpr uint8_t slave_address = 0x55;
+
+}
+
+#endif  // LIB_DEFS_DEFS_HPP
--- firmware/main/src/main.cpp
+++ firmware/main/src/main.cpp
@@ -3,6 +3,9 @@
 
 int main() {
     stdio_init_all();
+
+    i2c_driver main_i2c(defs::i2c::inst, defs::i2c::sda_pin, defs::i2c::scl_pin, defs::i2c::slave_address, defs::i2c_baudrate);
+
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
