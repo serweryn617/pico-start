cf3f774e94237a05188483a2f1e40c9cb85cf2b9
serweryn617 <seweryn.r.1999@gmail.com>
2025-06-20 16:48:01 +0200

    Add UART driver

--- firmware/lib/defs/inc/defs/defs.hpp
+++ firmware/lib/defs/inc/defs/defs.hpp
@@ -3,14 +3,27 @@
 
 #include <cstdint>
 
-namespace defs::i2c {
+namespace defs {
 
-constexpr uint8_t inst = 1;
-constexpr uint8_t scl_pin = 3;
-constexpr uint8_t sda_pin = 2;
+namespace i2c {
+
+constexpr uint8_t inst = 0;
+constexpr uint8_t scl = 13;
+constexpr uint8_t sda = 12;
 constexpr uint32_t baudrate = 400'000;
 constexpr uint8_t slave_address = 0x55;
 
-}
+}  // namespace i2c
+
+namespace uart {
+
+constexpr uint8_t inst = 1;
+constexpr uint8_t rx = 9;
+constexpr uint8_t tx = 8;
+constexpr uint32_t baudrate = 9600;
+
+}  // namespace uart
+
+}  // namespace defs
 
 #endif  // LIB_DEFS_DEFS_HPP
--- firmware/main/src/main.cpp
+++ firmware/main/src/main.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "i2c/i2c_driver.h"
+#include "uart/uart_driver.h"
+#include "defs/defs.hpp"
+
+using namespace drivers::i2c;
+using namespace drivers::uart;
+
 int main() {
     stdio_init_all();
 
-    i2c_driver main_i2c(defs::i2c::inst, defs::i2c::sda_pin, defs::i2c::scl_pin, defs::i2c::slave_address, defs::i2c_baudrate);
+    i2c_driver main_i2c(defs::i2c::inst, defs::i2c::sda, defs::i2c::scl, defs::i2c::slave_address, defs::i2c::baudrate);
+    uart_driver main_uart(defs::uart::inst, defs::uart::rx, defs::uart::tx, defs::uart::baudrate);
 
     while (true) {
         printf("Hello, world!\n");
